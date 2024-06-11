58670f228614ca33d2eea92a102c7e802aedfd8e
Tulio Lemos <tlcbrasil164@gmail.com>
2024-06-11 12:57:49 -0300

    feat: Consuming the cards microservice!

---
+++ mscredit-appraiser/src/main/java/br/com/mscredit_appraiser/client/CardResourceClient.java
@@ -0,0 +1,16 @@
+package br.com.mscredit_appraiser.client;
+
+import br.com.mscredit_appraiser.model.ClientCard;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscards", path = "/cards")
+public interface CardResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<ClientCard>> findCardsByCpf(@RequestParam("cpf") String cpf);
+}
--- mscredit-appraiser/src/main/java/br/com/mscredit_appraiser/service/CreditAppraiserService.java
+++ mscredit-appraiser/src/main/java/br/com/mscredit_appraiser/service/CreditAppraiserService.java
@@ -1,11 +1,15 @@
 package br.com.mscredit_appraiser.service;
 
+import br.com.mscredit_appraiser.client.CardResourceClient;
 import br.com.mscredit_appraiser.client.ClientResourceClient;
+import br.com.mscredit_appraiser.model.ClientCard;
 import br.com.mscredit_appraiser.model.ClientData;
 import br.com.mscredit_appraiser.model.ClientSituation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CreditAppraiserService {
@@ -13,10 +17,16 @@ public class CreditAppraiserService {
     @Autowired
     private ClientResourceClient clientResource;
 
+    @Autowired
+    private CardResourceClient cardResource;
+
     public ClientSituation getClientSituation(String cpf) {
+        ResponseEntity<ClientData> clientData = clientResource.findByCpf(cpf);
+        ResponseEntity<List<ClientCard>> clientCard = cardResource.findCardsByCpf(cpf);
 
-        ResponseEntity<ClientData> response = clientResource.findByCpf(cpf);
-
-        return ClientSituation.builder().client(response.getBody()).build();
+        return ClientSituation.builder()
+                .client(clientData.getBody())
+                .cards(clientCard.getBody())
+                .build();
     }
 }
